83eb819b2db6d2375cae6d59b85ef56a8d86c5cb
Denis Markov <keynsianets@gmail.com>
2018-07-23 15:31:39 +0300

    Add .gitignore file

---
+++ TableViewAndSearchBarPractice/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TableViewAndSearchBarPractice
+//
+//  Created by Denis Markov on 7/23/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import Foundation
---
+++ TableViewAndSearchBarPractice/RestaurantModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainTableDataModel.swift
+//  TableViewAndSearchBarPractice
+//
+//  Created by Denis Markov on 7/23/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import Foundation
+
+struct RestaurantModel {
+    let imageName: String?
+    let header: String?
+    let adress: String?
+}
